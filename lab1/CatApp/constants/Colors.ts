0d629dce64d6710b455dbb465e2c8509b469dfb6
AgniaStaravoitava <ahniya_staravoitava@epam.com>
2022-05-12 21:16:28 +0300

    add orange header

--- lab1/CatApp/constants/Colors.ts
+++ lab1/CatApp/constants/Colors.ts
@@ -1,19 +1,23 @@
-const tintColorLight = '#2f95dc';
-const tintColorDark = '#fff';
+const tintColorLight = "#2f95dc";
+const tintColorDark = "#fff";
 
 export default {
   light: {
-    text: '#000',
-    background: '#fff',
+    text: "#000",
+    textInverse: "#fff",
+    background: "#fff",
+    backgroundDark: "orange",
     tint: tintColorLight,
-    tabIconDefault: '#ccc',
+    tabIconDefault: "#ccc",
     tabIconSelected: tintColorLight,
   },
   dark: {
-    text: '#fff',
-    background: '#000',
+    text: "#fff",
+    textInverse: "#000",
+    background: "#000",
+    backgroundDark: "black",
     tint: tintColorDark,
-    tabIconDefault: '#ccc',
+    tabIconDefault: "#ccc",
     tabIconSelected: tintColorDark,
   },
 };
