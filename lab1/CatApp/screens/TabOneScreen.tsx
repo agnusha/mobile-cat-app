45fcfcb8271d954b4786e74cdb262a81a9102752
AgniaStaravoitava <ahniya_staravoitava@epam.com>
2022-05-13 12:26:43 +0300

    add img

--- lab1/CatApp/screens/TabOneScreen.tsx
+++ lab1/CatApp/screens/TabOneScreen.tsx
@@ -1,7 +1,6 @@
 import { StyleSheet } from "react-native";
-
 import EditScreenInfo from "../components/EditScreenInfo";
-import { Text, View } from "../components/Themed";
+import { View } from "../components/Themed";
 import { RootTabScreenProps } from "../types";
 
 export default function TabOneScreen({
@@ -9,12 +8,6 @@ export default function TabOneScreen({
 }: RootTabScreenProps<"TabOne">) {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Hello</Text>
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
-      />
       <EditScreenInfo path="/screens/TabOneScreen.tsx" />
     </View>
   );
@@ -24,15 +17,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
-    justifyContent: "center",
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
+    padding: 15,
   },
 });
