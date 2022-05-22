506238b42d616cb030e7420c184016393eff838d
AgniaStaravoitava <ahniya_staravoitava@epam.com>
2022-05-22 23:04:52 +0300

    add fav item

--- lab1/CatApp/components/GuitarsScreen.tsx
+++ lab1/CatApp/components/GuitarsScreen.tsx
@@ -1,158 +1,37 @@
-import {
-  AntDesign,
-  MaterialCommunityIcons,
-  SimpleLineIcons,
-} from "@expo/vector-icons";
 import React from "react";
-import { StyleSheet, TouchableOpacity } from "react-native";
-import mainImg from "../assets/images/pushnoi.jpg";
-import { Text, View } from "./Themed";
+import { StyleSheet } from "react-native";
+import GuitarItem from "./GuitarItemRow";
 
 export default function GuitarsScreen({ path }: { path: string }) {
+  var items = [
+    {
+      id: 1,
+      name: "Guitar 1",
+      description: "Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet",
+    },
+    {
+      id: 2,
+      name: "Guitar 2",
+      description:
+        "Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum  dolor sit amet",
+    },
+    {
+      id: 3,
+      name: "Guitar 3",
+      description:
+        "Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum  dolor sit amet",
+    },
+  ];
   return (
     <>
-      <TouchableOpacity
-        style={{
-          flexDirection: "row",
-          backgroundColor: "#fff",
-          marginBottom: 2,
-          height: 80,
-          width: "100%",
-        }}
-        onPress={() => {}}
-      >
-        <View style={[styles.centerElement, { width: 75 }]}>
-          <View
-            style={{
-              borderWidth: 1,
-              borderColor: "#ececec",
-              padding: 7,
-              borderRadius: 25,
-            }}
-          >
-            <AntDesign name="tago" size={28} color="#e89b17" />
-          </View>
-        </View>
-        <View style={{ flexGrow: 1, flexShrink: 1, alignSelf: "center" }}>
-          <Text style={{ fontSize: 15 }}>Promotions</Text>
-          <Text numberOfLines={1} style={{ color: "#8f8f8f" }}>
-            Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum
-            dolor sit amet
-          </Text>
-        </View>
-        <View style={[styles.centerElement, { width: 40 }]}>
-          <Text
-            style={{
-              backgroundColor: "#ee4d2d",
-              color: "#fff",
-              borderRadius: 8,
-              paddingVertical: 3,
-              paddingHorizontal: 5,
-              fontSize: 10,
-            }}
-          >
-            240
-          </Text>
-        </View>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={{
-          flexDirection: "row",
-          backgroundColor: "#fff",
-          marginBottom: 2,
-          height: 80,
-          width: "100%",
-        }}
-        onPress={() => {}}
-      >
-        <View style={[styles.centerElement, { width: 75 }]}>
-          <View
-            style={{
-              borderWidth: 1,
-              borderColor: "#ececec",
-              padding: 7,
-              borderRadius: 25,
-            }}
-          >
-            <MaterialCommunityIcons
-              name="wunderlist"
-              size={28}
-              color="#26aa99"
-            />
-          </View>
-        </View>
-        <View style={{ flexGrow: 1, flexShrink: 1, alignSelf: "center" }}>
-          <Text style={{ fontSize: 15 }}>Social Updates</Text>
-          <Text numberOfLines={1} style={{ color: "#8f8f8f" }}>
-            Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum
-            dolor sit amet
-          </Text>
-        </View>
-        <View style={[styles.centerElement, { width: 40 }]}>
-          <Text
-            style={{
-              backgroundColor: "#ee4d2d",
-              color: "#fff",
-              borderRadius: 5,
-              paddingVertical: 3,
-              paddingHorizontal: 5,
-              fontSize: 10,
-            }}
-          >
-            99
-          </Text>
-        </View>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={{
-          flexDirection: "row",
-          backgroundColor: "#fff",
-          marginBottom: 2,
-          height: 80,
-          width: "100%",
-        }}
-        onPress={() => {}}
-      >
-        <View style={[styles.centerElement, { width: 75 }]}>
-          <View
-            style={{
-              borderWidth: 1,
-              borderColor: "#ececec",
-              padding: 7,
-              borderRadius: 25,
-            }}
-          >
-            <SimpleLineIcons name="handbag" size={28} color="#ef5b3d" />
-          </View>
-        </View>
-        <View style={{ flexGrow: 1, flexShrink: 1, alignSelf: "center" }}>
-          <Text style={{ fontSize: 15 }}>MyEcommerce Updates</Text>
-          <Text numberOfLines={1} style={{ color: "#8f8f8f" }}>
-            Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum
-            dolor sit amet
-          </Text>
-        </View>
-        <View style={[styles.centerElement, { width: 40 }]}>
-          <Text
-            style={{
-              backgroundColor: "#ee4d2d",
-              color: "#fff",
-              borderRadius: 5,
-              paddingVertical: 3,
-              paddingHorizontal: 5,
-              fontSize: 10,
-            }}
-          >
-            7
-          </Text>
-        </View>
-      </TouchableOpacity>
+      {items.map((i) => (
+        <GuitarItem item={i} />
+      ))}
     </>
   );
 }
 
 const styles = StyleSheet.create({
-  centerElement: { justifyContent: "center", alignItems: "center" },
   container: {
     flex: 1,
     paddingVertical: 50,
@@ -162,22 +41,5 @@ const styles = StyleSheet.create({
 
   imageContainer: {
     alignSelf: "center",
-  },
-
-  marginContainer: {
-    alignItems: "center",
-    marginHorizontal: 50,
-  },
-
-  baseText: {
-    fontSize: 17,
-    lineHeight: 24,
-    textAlign: "center",
-  },
-
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
   },
 });
