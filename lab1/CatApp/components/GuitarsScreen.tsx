b0e40638db8cc54836e768fa5efe2895634dca11
AgniaStaravoitava <ahniya_staravoitava@epam.com>
2022-05-28 11:21:01 +0300

    extract to const

--- lab1/CatApp/components/GuitarsScreen.tsx
+++ lab1/CatApp/components/GuitarsScreen.tsx
@@ -1,67 +1,19 @@
-import { Tab, TabView } from "@rneui/themed";
+import { Dialog } from "@rneui/base";
+import { Avatar, Tab, TabView } from "@rneui/themed";
 import React from "react";
 import { StyleSheet, View } from "react-native";
-import GuitarItem from "./GuitarItemRow";
+import { Guitars } from "../constants/Guitars";
+import GuitarItemRow from "./GuitarItemRow";
 
-export default function GuitarsScreen({}: {}) {
+export default function GuitarsScreen() {
   const [index, setIndex] = React.useState(0);
+  const [visible, setVisible] = React.useState(true);
+  const items = Guitars;
 
-  var items = [
-    [
-      {
-        id: 1,
-        name: "HOFNER SUPER SOLID V2",
-        description:
-          "His first guitar was a HOFNER SUPER SOLID V2 with a red body and two Diamond Logo 510 pickups and is still in the Knopfler collection.",
-        image: require("../assets/images/1.png"),
-      },
-      {
-        id: 2,
-        name: "GIBSON LES PAUL SPECIAL DOUBLE CUTAWAY",
-        description:
-          "After it for a long time (50-70s) Mark played the GIBSON LES PAUL SPECIAL DOUBLE CUTAWAY guitar, which has a cherry-colored mahogany body and two P-90 pickups.",
-        image: require("../assets/images/2.jpg"),
-      },
-      {
-        id: 3,
-        name: "FENDER STRATOCASTER",
-        description:
-          "It was not until the late 70s that Mark Knopfler became the owner of the guitar that is associated with him and his playing, being his symbol - the red FENDER STRATOCASTER.",
-        image: require("../assets/images/3.jpeg"),
-      },
-      {
-        id: 4,
-        name: "GIBSON LES PAUL STANDARD",
-        description: "In parallel, Mark plays GIBSON LES PAUL STANDARD.",
-        image: require("../assets/images/4.jpg"),
-      },
-    ],
-    [
-      {
-        id: 5,
-        name: "FENDER STRATOCASTER",
-        description:
-          'Eric switched to a FENDER STRATOCASTER in 1967, acquiring an instrument dubbed the "Brownie", although the neck was temporarily relocated to a FENDER TELECASTER, but then returned to its place.',
-        image: require("../assets/images/3.jpeg"),
-      },
-      {
-        id: 6,
-        name: "GIBSON LES PAUL SPECIAL DOUBLE CUTAWAY",
-        description:
-          'in 1973, he assembled it from four different FENDER STRATOCASTER guitars from the 50s to his new "favorite" - a guitar with the name "Blackie".',
-        image: require("../assets/images/6.jpg"),
-      },
-    ],
-    [
-      {
-        id: 7,
-        name: "Black Strat",
-        description:
-          "David Gilmour had a decent collection of guitars, but the Black Strat, a modified black FENDER STRATOCASTER, held a special place in it.",
-        image: require("../assets/images/7.jpg"),
-      },
-    ],
-  ];
+  const toggleDialog = () => {
+    setVisible(!visible);
+  };
+
   return (
     <>
       <>
@@ -114,26 +66,39 @@ export default function GuitarsScreen({}: {}) {
           <TabView.Item style={{ width: "100%" }}>
             <View style={{ width: "100%" }}>
               {items[0].map((i) => (
-                <GuitarItem item={i} />
+                <GuitarItemRow item={i} />
               ))}
             </View>
           </TabView.Item>
           <TabView.Item style={{ width: "100%" }}>
             <View style={{ width: "100%" }}>
               {items[1].map((i) => (
-                <GuitarItem item={i} />
+                <GuitarItemRow item={i} />
               ))}
             </View>
           </TabView.Item>
           <TabView.Item style={{ width: "100%" }}>
             <View>
               {items[2].map((i) => (
-                <GuitarItem item={i} />
+                <GuitarItemRow item={i} />
               ))}
             </View>
           </TabView.Item>
         </TabView>
       </>
+
+      <Dialog isVisible={visible} onBackdropPress={toggleDialog}>
+        <Dialog.Title title="You find a toad" />
+        <Avatar
+          rounded
+          source={{
+            uri: "https://ru.meming.world/images/ru/thumb/e/e8/%D0%9F%D0%BE%D0%BB%D0%BA%D0%BE%D0%B2%D0%BD%D0%B8%D0%BA_%D0%96%D0%B0%D0%B1%D0%B0_%D1%88%D0%B0%D0%B1%D0%BB%D0%BE%D0%BD.jpeg/300px-%D0%9F%D0%BE%D0%BB%D0%BA%D0%BE%D0%B2%D0%BD%D0%B8%D0%BA_%D0%96%D0%B0%D0%B1%D0%B0_%D1%88%D0%B0%D0%B1%D0%BB%D0%BE%D0%BD.jpeg",
+          }}
+        />
+        <Dialog.Actions>
+          <Dialog.Button title="CANCEL" onPress={toggleDialog} />
+        </Dialog.Actions>
+      </Dialog>
     </>
   );
 }
---
+++ lab1/CatApp/constants/Guitars.ts
@@ -0,0 +1,56 @@
+export const Guitars = [
+  [
+    {
+      id: 1,
+      name: "HOFNER SUPER SOLID V2",
+      description:
+        "His first guitar was a HOFNER SUPER SOLID V2 with a red body and two Diamond Logo 510 pickups and is still in the Knopfler collection.",
+      image: require("../assets/images/1.png"),
+    },
+    {
+      id: 2,
+      name: "GIBSON LES PAUL SPECIAL DOUBLE CUTAWAY",
+      description:
+        "After it for a long time (50-70s) Mark played the GIBSON LES PAUL SPECIAL DOUBLE CUTAWAY guitar, which has a cherry-colored mahogany body and two P-90 pickups.",
+      image: require("../assets/images/2.jpg"),
+    },
+    {
+      id: 3,
+      name: "FENDER STRATOCASTER",
+      description:
+        "It was not until the late 70s that Mark Knopfler became the owner of the guitar that is associated with him and his playing, being his symbol - the red FENDER STRATOCASTER.",
+      image: require("../assets/images/3.jpeg"),
+    },
+    {
+      id: 4,
+      name: "GIBSON LES PAUL STANDARD",
+      description: "In parallel, Mark plays GIBSON LES PAUL STANDARD.",
+      image: require("../assets/images/4.jpg"),
+    },
+  ],
+  [
+    {
+      id: 5,
+      name: "FENDER STRATOCASTER",
+      description:
+        'Eric switched to a FENDER STRATOCASTER in 1967, acquiring an instrument dubbed the "Brownie", although the neck was temporarily relocated to a FENDER TELECASTER, but then returned to its place.',
+      image: require("../assets/images/3.jpeg"),
+    },
+    {
+      id: 6,
+      name: "GIBSON LES PAUL SPECIAL DOUBLE CUTAWAY",
+      description:
+        'in 1973, he assembled it from four different FENDER STRATOCASTER guitars from the 50s to his new "favorite" - a guitar with the name "Blackie".',
+      image: require("../assets/images/6.jpg"),
+    },
+  ],
+  [
+    {
+      id: 7,
+      name: "Black Strat",
+      description:
+        "David Gilmour had a decent collection of guitars, but the Black Strat, a modified black FENDER STRATOCASTER, held a special place in it.",
+      image: require("../assets/images/7.jpg"),
+    },
+  ],
+];
